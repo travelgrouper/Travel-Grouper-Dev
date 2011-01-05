53209add2dc147637d3601b430c8bb1e022834d8
Travelgrouper <matthew.guidi@travelgrouper.com>
2011-01-05 14:18:05 -0700

    changing

---
+++ db/migrate/20110105205710_create_facebook_hash_in_users.rb
@@ -0,0 +1,9 @@
+class CreateFacebookHashInUsers < ActiveRecord::Migration
+  def self.up
+    add_column :users, :facebook_hash, :string
+  end
+
+  def self.down
+    remove_column :users, :facebook_hash
+  end
+end
