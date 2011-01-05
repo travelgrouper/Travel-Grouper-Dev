f0aca0287ba6ef77fbddfc65ada07bc1c018a036
Travelgrouper <matthew.guidi@travelgrouper.com>
2011-01-04 17:45:51 -0700

    Initial Commit

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, '157820100932662', '1b212fea35d10344f5a037dd89cb6d5b', :scope => 'email,offline_access'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravelGrouperDev::Application.config.secret_token = 'bc406ef8493c21cf97409838b55f0473137d94bace970b1d670e9c86348398cf3f3c2b864d09240d614980d81937eb3a71e311be30774eff5710b8992f6cde33'
